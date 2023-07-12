fc3b88f721178e64437917db41694029beb71aa4
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2023-07-12 21:19:54 +0530

    updated faqs

--- src/components/FAQs/data.ts
+++ src/components/FAQs/data.ts
@@ -1,52 +1,47 @@
 export const faqDat = [
-    {
-        id:1 ,
-        que: "What do you understand by the term, the robotics?",
-        ans: "The robotics is a combined branch of engineering and science which deals with the study of development,operation, and control of intelligent robots. The robotics is a part of Artificial intelligence."
-    },
-    {
-        id:2 ,
-        que : "",
-        ans: ""
-    },
-    {
-        id:3 ,
-        que : "",
-        ans: ""
-    },
-    {
-        id:4 ,
-        que : "",
-        ans: ""
-    },
-    {
-        id:5 ,
-        que : "",
-        ans: ""
-    },
-    {
-        id:6 ,
-        que : "",
-        ans: ""
-    },
-    {
-        id:6 ,
-        que : "",
-        ans: ""
-    },
-    {
-        id:6 ,
-        que : "",
-        ans: ""
-    },
-    {
-        id:6 ,
-        que : "",
-        ans: ""
-    },
-    {
-        id:6 ,
-        que : "",
-        ans: ""
-    },
-]
+  {
+    id: 1,
+    que: "Who can join the club?",
+    ans: "The club is open to all engineering students enrolled in RCOEM who have an interest in robotics and aviation, regardless of their specific engineering discipline.",
+  },
+  {
+    id: 2,
+    que: "Do I need any prior experience in robotics or aviation to join?",
+    ans: "No, prior experience is not necessary. We welcome members with all levels of experience, from beginners to advanced enthusiasts. Our club is designed to cater to students at various skill levels.",
+  },
+  {
+    id: 3,
+    que: "How can I become a member of the club?",
+    ans: "To become a member the individual has to appear for club recruitment procedures.",
+  },
+  {
+    id: 4,
+    que: "Does the club participate in national/international competitions?",
+    ans: "Certainly! The teams from our club have participated in various national competitions like Axis VNIT, Techfest organized by IITs throughout the country and international competitions like Technoxian (organized by IRC), Abu Robocon, competitions organized by SAE Baja, etc.",
+  },
+  {
+    id: 5,
+    que: "What kind of activities does the club organize?",
+    ans: "The club organizes a wide range of activities, including workshops, training sessions, competitions, and projects. These activities cover various aspects of robotics and aviation.",
+  },
+  {
+    id: 6,
+    que: "Can I participate in competitions even if I am new to robotics and aviation?",
+    ans: "Absolutely! Our competitions are designed to encourage participation from students at all levels. We have different categories to cater to beginners as well as those with more advanced skills.",
+  },
+  {
+    id: 7,
+    que: "How can I stay updated with club events and activities?",
+    ans: "We regularly update our website and social media handles with the latest news, events, and announcements. To know more, follow us on social media platforms like Instagram and LinkedIn.",
+  },
+  {
+    id: 8,
+    que: "Can I propose and work on my own project within the club?",
+    ans: "Yes, we encourage members to work on their own projects. Whether it's a personal project or a team effort, we provide a supportive environment for you to explore your ideas and bring them to life.",
+  },
+  {
+    id: 9,
+    que: "Are there any networking opportunities through the club?",
+    ans: "Yes, being part of the club offers excellent networking opportunities. You'll have the chance to connect with fellow students, faculty members, and our exceptional alumni.",
+  }
+];
